e714c50c488c1564c7725ea9e89c14de67adc831
martinaesposito <martina.esposito@mail.polimi.it>
2022-09-23 16:27:12 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,17 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  rectMode=
+  noFill();
+  strokeCap(SQUARE);
+  stroke(30);
+  strokeWeight(10);
+
 }
 
 function draw() {
+
+ circle(30,1,30,40);
+
 
 
 }
