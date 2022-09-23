c171a46ca95baa8a24e6d4709dc703e838c31c82
martinaesposito <martina.esposito@mail.polimi.it>
2022-09-23 17:24:53 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,14 +4,41 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
-
 }
+
+let offset = 0;
+const inc= 0.005
 
 function draw() {
 
-translate(50, 50);
-fill= 30;
-rect(width/2,height/2,50,50);
 
+
+
+push();
+
+for(i=0; i<=255; i++){
+
+  let r=i*noise(offset);
+  let g=i*noise(offset);
+  let b=i*noise(offset);
+
+  color= (r,g,b);
+}
+
+fill= color;
+
+let xrect= width/2;
+let yrect= height/2;
+
+let xrectnew= xrect*noise(offset);
+let yrectnew= yrect*noise(offset);
+
+rect(xrect, yrect, 50, 50);
+
+for (i=0; i<=width; i++) {
+   
+  rect(50+ xrectnew+frameCount ,xrectnew+frameCount, 50, 50)
+}
+ 
 
 }
