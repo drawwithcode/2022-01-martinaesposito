72a84c2b58ac0533c27ef9cebad1855438c7cd3e
martinaesposito <martina.esposito@mail.polimi.it>
2022-09-24 08:57:34 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,35 +9,29 @@ function setup() {
 let offset = 0;
 const inc= 0.005
 
-function draw() {
 
-
-
-
-push();
-
-for(i=0; i<=255; i++){
-
-  let r=i*noise(offset);
-  let g=i*noise(offset);
-  let b=i*noise(offset);
-
-  color= (r,g,b);
+function color() {
+  for(i=0; i<=255; i++){
+    let r=i*noise(offset);
+    let g=i*noise(offset);
+    let b=i*noise(offset);
+    
+    color= (r,g,b);
+  }
 }
 
 fill= color;
 
-let xrect= width/2;
-let yrect= height/2;
+function draw() {
 
-let xrectnew= xrect*noise(offset);
-let yrectnew= yrect*noise(offset);
-
-rect(xrect, yrect, 50, 50);
+let xrectnew= width*noise(offset);
+let yrectnew= height*noise(offset); 
 
 for (i=0; i<=width; i++) {
    
-  rect(50+ xrectnew+frameCount ,xrectnew+frameCount, 50, 50)
+  rect(xrectnew+ frameCount ,xrectnew+ frameCount, 100*noise(offset), 50*noise(offset))
+
+  offset += inc; 
 }
  
 
